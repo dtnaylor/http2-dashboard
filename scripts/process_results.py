c67041e421feb3eaa34455d86aa81a85f600374e
David Naylor <dnaylor@cs.cmu.edu>
2015-04-17 17:16:17 -0400

    Plots for support over time

--- scripts/process_results.py
+++ scripts/process_results.py
@@ -9,8 +9,15 @@ import sys
 import argparse
 import logging
 import glob
+import datetime
+import json
+
+from collections import defaultdict
 
 
+##
+## HELPER FUNCTIONS
+##
 def setup_logging():
     if args.quiet:
         level = logging.WARNING
@@ -35,23 +42,91 @@ def load_conf(conf_file):
 
     return conf
 
-def advertised_support_by_date(result_file, out_file):
-    print result_file, out_file
+def read_time_series(filepath):
+    try:
+        with open(filepath, 'r') as f:
+            counts = []
+            start_date = None
+            last_date = None
+            for line in f:
+                # parse the line
+                if line.strip() == '': continue
+                count, date = line.strip().split()
+                count = int(count)
+                date = datetime.datetime.strptime(date, '%a_%b_%d_%Y')
+
+                # if this is the first entry, set start_date
+                if not start_date:
+                    start_date = date
+
+                # check for duplicate or missing days
+                if last_date:
+                    days_since_last_point = (date-last_date).days
+
+                    # ignore this point if 2nd point this day
+                    if days_since_last_point == 0:
+                        continue
+
+                    # repeat last data point if days are missing
+                    if days_since_last_point > 1:
+                        counts += [counts[-1]]*(days_since_last_point-1)
+
+                counts.append(count)
+                last_date = date
+
+        return counts, start_date, 24 * 3600 * 1000
+    except Exception:
+        logging.exception('Error reading time series: %s' % filepath)
+        return None, None, None
+
+##
+## DATA PROCESSING FUNCTIONS
+##
+def support_by_date(conf, out_file):
+    series = ['advertised_support_by_date',
+              'actual_support_by_date',
+              'h2_12_advertised_support_by_date',
+              'h2_14_advertised_support_by_date',
+              'h2_15_advertised_support_by_date',
+              'h2_16_advertised_support_by_date',
+              'h2_17_advertised_support_by_date',
+              'spdy_2',
+              'spdy_3',
+              'spdy_3.1',
+             ]
+    
+    # series name -> key (counts, start, interval) -> value
+    data = defaultdict(dict)
+
+    for series_name in series:
+        counts, start_date, interval =\
+            read_time_series(conf[series_name])
+
+        data[series_name]['counts'] = counts
+        data[series_name]['start_year'] = start_date.year
+        data[series_name]['start_month'] = start_date.month-1
+        data[series_name]['start_day'] = start_date.day
+        data[series_name]['interval'] = interval
+
+    with open(out_file, 'w') as f:
+        json.dump(data, f)
 
 
 
 
 
+##
+## MAIN
+##
 def main():
 
     # make output data dir if it doesn't exist
     if not os.path.exists(args.outdir):
         os.makedirs(args.outdir)
     
-    # advertised support by date
-    if os.path.isfile(conf['advertised_support_by_date']):
-        out_file = os.path.join(args.outdir, 'advertised_support_by_date.json')
-        advertised_support_by_date(conf['advertised_support_by_date'], out_file)
+    # SUPPORT BY DATE
+    out_file = os.path.join(args.outdir, 'support_by_date.json')
+    support_by_date(conf, out_file)
         
 
 
---
+++ www/js/measurements/adoption.js
@@ -0,0 +1,83 @@
+$.getJSON('/data/support_by_date.json', function(data) {
+	plot_time_series('#advertised-vs-actual-chart',
+		'Announced Support vs. Actual Support',
+		data,
+		['advertised_support_by_date', 'actual_support_by_date'],
+		['Announced Support', 'Actual Support']);
+	
+	plot_time_series('#draft-versions-chart',
+		'Version Comparison',
+		data,
+		[
+		'spdy_2',
+		'spdy_3',
+		'spdy_3.1',
+		'h2_12_advertised_support_by_date',
+		'h2_14_advertised_support_by_date',
+		'h2_15_advertised_support_by_date',
+		'h2_16_advertised_support_by_date',
+		'h2_17_advertised_support_by_date'],
+		['SPDY 2', 'SPDY 3', 'SPDY 3.1',
+		 'H2 Draft 12', 'H2 Draft 14', 'H2 Draft 15', 'H2 Draft 16', 'H2 Draft 17']
+		);
+
+});
+
+
+
+function plot_time_series(container, title, data, series_keys, series_labels) {
+	// Build highcharts series entries
+	series = [];
+	for (var i = 0; i < series_keys.length; i++) {
+		var key = series_keys[i];
+		series.push({
+			type: 'line',
+			name: series_labels[i],
+			pointInterval: data[key]['interval'],
+			pointStart: Date.UTC(data[key]['start_year'],
+								 data[key]['start_month'], 
+								 data[key]['start_day']),
+			data: data[key]['counts']
+		});
+	}
+
+	$(function () {
+		$(container).highcharts({
+			chart: {
+				zoomType: 'x'
+			},
+			title: {
+				text: title,
+			},
+			subtitle: {
+				text: document.ontouchstart === undefined ?
+						'Click and drag in the plot area to zoom in' :
+						'Pinch the chart to zoom in'
+			},
+			xAxis: {
+				type: 'datetime',
+				minRange: 14 * 24 * 3600000 // fourteen days
+			},
+			yAxis: {
+				min: 0,
+				//type: 'logarithmic',
+				title: {
+					text: 'Number of Domains'
+				}
+			},
+			tooltip: {
+				shared: true,
+				valueSuffix: ' domains',
+			},
+			legend: {
+				enabled: true,
+			},
+			plotOptions: {
+
+			},
+
+			series: series,
+		});
+	});
+
+}
