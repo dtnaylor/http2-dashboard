cc4a90ab807b004a8ad2fb4d424a0602400203cd
David Naylor <dnaylor@cs.cmu.edu>
2015-07-27 22:37:35 -0400

    Sync to AWS only syncs site, not data

--- scripts/sync_to_s3.sh
+++ scripts/sync_to_s3.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-aws s3 sync --exclude ../www/_site/data ../www/_site/ s3://isthewebhttp2yet.com
+rm -r ../www/_site/data
+aws s3 sync ../www/_site/ s3://isthewebhttp2yet.com
+
+# --exclude doesn't seem to be working
+# aws s3 sync ../www/_site/ s3://isthewebhttp2yet.com --exclude "../www/_site/data/*"
